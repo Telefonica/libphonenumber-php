b77d1fc2b5d3cbdf6123f4f9a146b66108e5b38e
Juan Cazalla Estrella <juan.cazallaestrella@tuenti.com>
2018-03-16 09:45:46 +0100

    CALL-1571 Added more tests

--- tests/Issues/EcuadorMetadataTest.php
+++ tests/Issues/EcuadorMetadataTest.php
@@ -19,10 +19,62 @@ class ErrorMetadataTest extends \PHPUnit_Framework_TestCase
         $this->phoneNumberUtil = PhoneNumberUtil::getInstance();
     }
 
-    public function testParseEcuadorNumberWithNewAcceptedPrefix()
+    public function testParseEcuadorNumberWithNewAcceptedPrefixWithCountryCode()
     {
-        $number = $this->phoneNumberUtil->parse('593962600575', 'EC');
+        $rawNumber = "593962600575";
+        $e164 = "+593962600575";
 
-        $this->assertEquals("+593962600575", $this->phoneNumberUtil->format($number, PhoneNumberFormat::E164));
+        $normalizedNumber = $this->whenParseRawNumber($rawNumber);
+
+        $this->thenNormalizedNumberIsValidWithExpectedE164($normalizedNumber, $e164);
+    }
+
+    public function testParseEcuadorNumberWithNewAcceptedPrefixWithCountryCodeAndStartingWithPlus()
+    {
+        $rawNumber = "+593962600575";
+        $e164 = "+593962600575";
+
+        $normalizedNumber = $this->whenParseRawNumber($rawNumber);
+
+        $this->thenNormalizedNumberIsValidWithExpectedE164($normalizedNumber, $e164);
+    }
+
+    public function testParseEcuadorNumberWithNewAcceptedPrefixWithoutCountryCode()
+    {
+        $rawNumber = "962600575";
+        $e164 = "+593962600575";
+
+        $normalizedNumber = $this->whenParseRawNumber($rawNumber);
+
+        $this->thenNormalizedNumberIsValidWithExpectedE164($normalizedNumber, $e164);
+    }
+
+    public function testParseEcuadorNumberWithNewAcceptedPrefixWithoutCountryCodeAndWithLeadingZero()
+    {
+        $rawNumber = "0962600575";
+        $e164 = "+593962600575";
+
+        $normalizedNumber = $this->whenParseRawNumber($rawNumber);
+
+        $this->thenNormalizedNumberIsValidWithExpectedE164($normalizedNumber, $e164);
+    }
+
+    /**
+     * @param $rawNumber
+     * @return \libphonenumber\PhoneNumber
+     */
+    private function whenParseRawNumber($rawNumber)
+    {
+        return $this->phoneNumberUtil->parse($rawNumber, "EC");
+    }
+
+    /**
+     * @param $normalizedNumber
+     * @param $e164
+     */
+    private function thenNormalizedNumberIsValidWithExpectedE164($normalizedNumber, $e164)
+    {
+        $this->assertEquals($e164, $this->phoneNumberUtil->format($normalizedNumber, PhoneNumberFormat::E164));
+        $this->assertEquals(true, $this->phoneNumberUtil->isValidNumber($normalizedNumber));
     }
 }
